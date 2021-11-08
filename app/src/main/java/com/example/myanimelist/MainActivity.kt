192ec8a47cc9a5659a3a2d7a274f484785f29e6a
aungnaioo24 <aungnaioo24@outlook.com>
2021-11-08 19:59:00 +0630

    Bottom Navbar Done and Change Launcher Icon

--- app/src/main/java/com/example/myanimelist/MainActivity.kt
+++ app/src/main/java/com/example/myanimelist/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myanimelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.myanimelist.fragment.MainListFragment
+import com.example.myanimelist.fragment.MyListFragment
+import com.example.myanimelist.fragment.ToWatchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main)
+
+        val mainListFragment = MainListFragment()
+        val myListFragment = MyListFragment()
+        val toWatchFragment = ToWatchFragment()
+
+        makeCurrentFragment(mainListFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.list -> makeCurrentFragment(mainListFragment)
+                R.id.my_list -> makeCurrentFragment(myListFragment)
+                R.id.to_watch -> makeCurrentFragment(toWatchFragment)
+            }
+            true
+        }
+
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.frame_layout_wrapper, fragment)
+        commit()
+    }
+
 }
