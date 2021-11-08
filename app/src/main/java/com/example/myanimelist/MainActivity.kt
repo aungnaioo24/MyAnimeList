17686f4b1e84f50d5b5c3d710c048651bce8a74c
aungnaioo24 <aungnaioo24@outlook.com>
2021-11-09 01:50:12 +0630

    update bottom Navbar with Navigation Component

--- app/src/main/java/com/example/myanimelist/MainActivity.kt
+++ app/src/main/java/com/example/myanimelist/MainActivity.kt
@@ -3,38 +3,24 @@ package com.example.myanimelist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.fragment.app.Fragment
-import com.example.myanimelist.fragment.MainListFragment
-import com.example.myanimelist.fragment.MyListFragment
-import com.example.myanimelist.fragment.ToWatchFragment
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myanimelist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val mainListFragment = MainListFragment()
-        val myListFragment = MyListFragment()
-        val toWatchFragment = ToWatchFragment()
+        val bottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.fragment)
 
-        makeCurrentFragment(mainListFragment)
-
-        bottom_navigation.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.list -> makeCurrentFragment(mainListFragment)
-                R.id.my_list -> makeCurrentFragment(myListFragment)
-                R.id.to_watch -> makeCurrentFragment(toWatchFragment)
-            }
-            true
-        }
-
-    }
-
-    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
-        replace(R.id.frame_layout_wrapper, fragment)
-        commit()
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 }
